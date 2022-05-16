553f7d262fc979ba07cd097ae7c3e5725b956a3d
Shashank <langishashank@gmail.com>
2022-05-16 18:18:58 +0530

    [Shashank]: Finding the Perfect Number.

---
+++ LogicalProblems/LogicalProblems/PerfectNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class PerfectNumber
+    {
+        public void Perfect()
+        {
+            int sum = 0, n;
+            Console.WriteLine("enter the Number");
+            int num = Convert.ToInt32(Console.ReadLine());
+            n = num;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("Entered number is a perfect number");                
+            }
+            else
+            {
+                Console.WriteLine("Entered number is not a perfect number");               
+            }
+        }
+    }
+}
--- LogicalProblems/LogicalProblems/Program.cs
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -12,6 +12,7 @@ namespace LogicalProblems
             Console.WriteLine("Enter the number for problem.\n");
             Console.WriteLine("1 Fibonacci Series.");
             Console.WriteLine("2 Prime Number.");
+            Console.WriteLine("3 Perfect Number.");
 
             int problem = Convert.ToInt32(Console.ReadLine());
             Program Logic = new Program();
@@ -25,8 +26,12 @@ namespace LogicalProblems
                     PrimeNumber prime = new PrimeNumber();
                     prime.Prime();
                     break;
+                case 3:
+                    PerfectNumber perfect = new PerfectNumber();
+                    perfect.Perfect();
+                    break;
                 default:
-                    Console.WriteLine("Enter the valid nmber from 1 to 2");
+                    Console.WriteLine("Enter the valid nmber from 1 to 3");
                     break;
 
             }
