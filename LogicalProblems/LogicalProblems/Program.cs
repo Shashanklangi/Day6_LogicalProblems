8f855c894f67972200b2ac6478c0d8281030f69d
Shashank <langishashank@gmail.com>
2022-05-18 12:23:18 +0530

    [Shashank]:Generating the Coupon Code

--- LogicalProblems/LogicalProblems/Program.cs
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -15,6 +15,7 @@ namespace LogicalProblems
             Console.WriteLine("3 Perfect Number.");
             Console.WriteLine("4 Reverse Number.");
             Console.WriteLine("5 Stop Watch.");
+            Console.WriteLine("6 Coupon Code.");
 
             int problem = Convert.ToInt32(Console.ReadLine());
             Program Logic = new Program();
@@ -40,8 +41,12 @@ namespace LogicalProblems
                     StopWatch stop = new StopWatch();
                     stop.Timer();
                     break;
+                case 6:
+                    CouponCode couponCode = new CouponCode();
+                    couponCode.Generator();
+                    break;
                 default:
-                    Console.WriteLine("Enter the valid nmber from 1 to 5");
+                    Console.WriteLine("Enter the valid nmber from 1 to 6");
                     break;
 
             }
